737e9c9bdbca3d673748c32a61b7e31eeb8df72d
mahmoud.31057513@ics.tanta.edu.eg <mahmoud.31057513@ics.tanta.edu.eg>
2024-03-18 14:44:39 +0200

    Add Relative Controller

---
+++ BrainMate/Controllers/RelativesController.cs
@@ -0,0 +1,18 @@
+﻿using BrainMate.Api.Base;
+using BrainMate.Core.Features.Relatives.Queries.Models;
+using BrainMate.Data.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainMate.Api.Controllers
+{
+	[ApiController]
+	public class RelativesController : AppControllerBase
+	{
+		[HttpGet(Routing.RelativesRouting.Paginated)]
+		public async Task<IActionResult> Paginated([FromQuery] GetRelativesPaginatedListQuery query)
+		{
+			var relatives = Ok(await _mediator.Send(query));
+			return relatives;
+		}
+	}
+}
