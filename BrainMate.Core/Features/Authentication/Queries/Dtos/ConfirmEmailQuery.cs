6f999e57913703edd035f27ac376b9f5fb0aa21d
mahmoud.31057513@ics.tanta.edu.eg <mahmoud.31057513@ics.tanta.edu.eg>
2024-03-28 12:07:52 +0200

    Confirm Email Check

---
+++ BrainMate.Core/Features/Authentication/Queries/Dtos/ConfirmEmailQuery.cs
@@ -0,0 +1,10 @@
+﻿using BrainMate.Core.Bases;
+using MediatR;
+namespace BrainMate.Core.Features.Authentication.Queries.Dtos
+{
+	public class ConfirmEmailQuery : IRequest<Response<string>>
+	{
+		public int UserId { get; set; }
+		public string? Code { get; set; }
+	}
+}
