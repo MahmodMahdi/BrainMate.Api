a93327a24aa1b13880137dbcfa71ccd848ff3ad9
mahmoud.31057513@ics.tanta.edu.eg <mahmoud.31057513@ics.tanta.edu.eg>
2024-04-01 23:44:43 +0200

    Event EndPoint PaginatedList

---
+++ BrainMate.Core/Features/Events/Queries/Models/GetEventsPaginatedListQuery.cs
@@ -0,0 +1,12 @@
+﻿using BrainMate.Core.Features.Events.Queries.Dtos;
+using BrainMate.Core.Wrappers;
+using MediatR;
+
+namespace BrainMate.Core.Features.Events.Queries.Models
+{
+	public class GetEventsPaginatedListQuery : IRequest<PaginateResult<GetEventsPaginatedListResponse>>
+	{
+		public int PageNumber { get; set; }
+		public int PageSize { get; set; }
+	}
+}
---
+++ BrainMate.Core/Mapping/Events/QueryMapping/GetEventPaginatedMapping.cs
@@ -0,0 +1,15 @@
+﻿using BrainMate.Core.Features.Events.Queries.Dtos;
+using BrainMate.Data.Entities;
+
+
+namespace BrainMate.Core.Mapping.Events
+{
+	public partial class EventProfile
+	{
+		public void GetEventsPaginationMapping()
+		{
+			CreateMap<Event, GetEventsPaginatedListResponse>()
+		   .ForMember(dest => dest.Task, op => op.MapFrom(src => src.Localize(src.TaskAr!, src.TaskEn!)));
+		}
+	}
+}
