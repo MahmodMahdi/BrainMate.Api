ea66e89efd20964fc783e14e98389be4f72b9e0e
mahmoud.31057513@ics.tanta.edu.eg <mahmoud.31057513@ics.tanta.edu.eg>
2024-04-24 15:31:31 +0200

    Updates in event queries

--- BrainMate.Core/Features/Events/Queries/Dtos/GetEventsPaginatedListResponse.cs
+++ BrainMate.Core/Features/Events/Queries/Dtos/GetEventsPaginatedListResponse.cs
@@ -1,9 +1,10 @@
 ﻿namespace BrainMate.Core.Features.Events.Queries.Dtos
 {
-	public class GetEventsPaginatedListResponse
-	{
-		public int Id { get; set; }
-		public string? Task { get; set; }
-		public TimeOnly? Time { get; set; }
-	}
+    public class GetEventsPaginatedListResponse
+    {
+        public int Id { get; set; }
+        public string? Task { get; set; }
+        public TimeOnly? Time { get; set; }
+        // public int PatientId { get; set; }
+    }
 }
